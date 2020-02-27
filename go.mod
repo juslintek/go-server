9ce48adba1afde5d6068aebebc9f9a335996a8a0
Linas Jusys <linas@webmobix.com>
2020-02-27 22:05:50 +0200

    feat: add base http handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module juslintek.dev/user/juslintek
+module juslintek/go-server
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.0
--- server.go
+++ server.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Just printing")
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Eik nx, %q", html.EscapeString(r.URL.Path))
+	})
+
+	s := &http.Server{
+		Addr:           ":8422",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
+
